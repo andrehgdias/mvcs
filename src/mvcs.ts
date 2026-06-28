8ba3516cb03413bb426a21b50e520e7185bbb478
André Dias <andredias@tuta.io>
2026-06-28 15:10:07 +0200

    Finish Init function

--- src/mvcs.ts
+++ src/mvcs.ts
@@ -1,15 +1,23 @@
 import path from "node:path";
 
 export const MVCS_REPOSITORY_NAME = ".mvcs";
+export const SNAPSHOTS_REPOSITORY_NAME = "snapshots";
 
 export type FsWrapper = {
-  mkdir: (path: string) => void;
+  mkdir: (path: string) => Promise<void>;
 };
 
 export async function init(fs: FsWrapper) {
-  const targetDirPath = path.join(process.cwd(), MVCS_REPOSITORY_NAME);
+  const mvcsDirPath = path.join(process.cwd(), MVCS_REPOSITORY_NAME);
+  console.log(`Creating MVCS repository at ${mvcsDirPath}`);
 
-  console.log(`Creating MVCS repository at ${targetDirPath}`);
+  try {
+    await fs.mkdir(mvcsDirPath);
+  } catch (e) {
+    throw new Error("Repository already initialized");
+  }
 
-  await fs.mkdir(targetDirPath);
+  const snapshotsDirPath = path.join(mvcsDirPath, SNAPSHOTS_REPOSITORY_NAME);
+  console.log(`Creating Snapshots repository at ${snapshotsDirPath}`);
+  await fs.mkdir(snapshotsDirPath);
 }
--- tests/init.test.ts
+++ tests/init.test.ts
@@ -1,25 +1,58 @@
 import assert from "assert";
-import { describe, it, mock } from "node:test";
-import { init, MVCS_REPOSITORY_NAME, type FsWrapper } from "../src/mvcs.js";
+import { beforeEach, describe, it, mock, type Mock } from "node:test";
+import {
+  init,
+  MVCS_REPOSITORY_NAME,
+  SNAPSHOTS_REPOSITORY_NAME,
+  type FsWrapper,
+} from "../src/mvcs.js";
 import path from "path";
 
 describe("init", function () {
-  it("creates .mvcs directory", async function () {
-    const mockMkdir = mock.fn((_path: string) => {});
-    const fs: FsWrapper = {
+  let mockMkdir: Mock<FsWrapper["mkdir"]>;
+  let fs: FsWrapper;
+
+  beforeEach(function () {
+    mockMkdir = mock.fn(async (_path: string) => {});
+    fs = {
       mkdir: mockMkdir,
     };
+  });
 
+  it("creates .mvcs directory", async function () {
     await init(fs);
 
-    assert.strictEqual(mockMkdir.mock.callCount(), 1);
+    assert.strictEqual(mockMkdir.mock.callCount(), 2);
 
-    const args = mockMkdir.mock.calls.at(0)!.arguments;
-    const pathComponents = path.parse(args.at(0)!);
+    const mvcsCallArgs = mockMkdir.mock.calls.at(0)!.arguments;
+    const pathComponents = path.parse(mvcsCallArgs.at(0)!);
     assert.strictEqual(pathComponents.name, MVCS_REPOSITORY_NAME);
   });
 
-  it.todo("creates .mvcs/snapshots directory", function () {});
+  it("creates .mvcs/snapshots directory", async function () {
+    await init(fs);
 
-  it.todo("fails when .mvcs alreday exists", function () {});
+    assert.strictEqual(mockMkdir.mock.callCount(), 2);
+
+    const snapshotsCallArgs = mockMkdir.mock.calls.at(1)!.arguments;
+    const pathComponents = path.parse(snapshotsCallArgs.at(0)!);
+    assert.strictEqual(
+      pathComponents.dir.split(path.sep).at(-1),
+      MVCS_REPOSITORY_NAME,
+    );
+    assert.strictEqual(pathComponents.name, SNAPSHOTS_REPOSITORY_NAME);
+  });
+
+  it("fails when repository is already initialized - .mvcs alreday exists", async function () {
+    mockMkdir = mock.fn((_path: string) => {
+      throw new Error("");
+    });
+    fs = {
+      mkdir: mockMkdir,
+    };
+
+    await assert.rejects(async function () {
+      await init(fs);
+    }, "/^Error: Repository already initialized$");
+  });
 });
